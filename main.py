ef7d9dc68b8dcd955b8c4fb494abc30a24202a09
Marcin Krupa <marcin.krupa@euvic.pl>
2022-01-15 22:01:25 +0100

    Change language

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import S
 import speech_recognition as sr
 import pyttsx3
 import datetime
@@ -9,14 +10,13 @@ import subprocess
 import wolframalpha
 import json
 import requests
-
-
-print('Loading your AI personal assistant - Jester')
+from googletrans import Translator
 
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
 engine.setProperty('voice','voices[0].id')
-
+wikipedia.set_lang("pl")
+translator = Translator()
 
 def speak(text):
     engine.say(text)
@@ -24,78 +24,90 @@ def speak(text):
 
 def wishMe():
     hour=datetime.datetime.now().hour
-    if hour>=0 and hour<12:
-        speak("Hello,Good Morning")
-        print("Hello,Good Morning")
-    elif hour>=12 and hour<18:
-        speak("Hello,Good Afternoon")
-        print("Hello,Good Afternoon")
+    if hour>=0 and hour<18:
+        if (hour % 2 == 0):
+            speak("Dzień dobry")
+            print("Dzień dobry")
+        else:
+            speak("Witaj")
+            print("Witaj")
     else:
-        speak("Hello,Good Evening")
-        print("Hello,Good Evening")
+        speak("Dobry wieczór")
+        print("Dobry wieczór")
 
 def takeCommand():
     r=sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening...")
+        print("Słucham...")
         audio=r.listen(source)
 
         try:
-            statement=r.recognize_google(audio,language='en-in')
+            statement=r.recognize_google(audio,language='pl-PL')
             print(f"user said:{statement}\n")
 
         except Exception as e:
-            speak("Pardon me, please say that again")
+            speak("Przepraszam, proszę powtórz jeszcze raz")
             return "None"
         return statement
 
-speak("Loading your AI personal assistant Jester")
+speak("Ładowanie twojego personalnego asystenta głosowego - Grażyna dwatysiące")
+print('Ładowanie twojego personalnego asystenta głosowego - Grażyna dwatysiące')
 wishMe()
 
 
 if __name__=='__main__':
-
+    closeStatements = ['żegnaj', 'ok pa', 'stop', 'zamknij', 'wyjdź']
+    wikiStatements = ['wikipedia', 'otwórz wikipedię', 'przeszukaj wikipedię', 'znajdź w wikipedii']
+    youtubeStatements = ['youtube', 'otwórz youtube']
+    googleStatements = ['google', 'otwórz google']
+    mailStatements = ['otwórz gmail', 'otwórz pocztę', 'poczta email', 'email']
+    weatherStatements = ['pogoda', 'jaka jest pogoda', 'powiedz mi jaka jest pogoda']
+    timeStatements = ['czas', 'jaka jest godzina', 'jaka jest teraz godzina', 'jaką mamy godzinę', 'która teraz jest godzina']
+    whoStatements = ['kim jesteś', 'co potrafisz', 'co umiesz', 'przedstaw się', 'powiedz coś o sobie']
+    buildStatements = ['kto cię stworzył', 'kto cię zbudował', 'kto cię wynalazł']
+    stackStatements = ['otwórz stack overflow', 'otwórz stackoverflow', 'stackoverflow', 'stack overflow']
+    newsStatements = ['news', 'newsy', 'informacje', 'wiadomości']
+    searchStatements = ['wyszukaj', 'znajdź']
+    logoutStatements = ['wyloguj']
 
     while True:
-        speak("Tell me how can I help you now?")
+        speak("Jak mogę Ci pomóc?")
         statement = takeCommand().lower()
         if statement==0:
             continue
 
-        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
-            speak('your personal assistant Jester is shutting down,Good bye')
-            print('your personal assistant Jester is shutting down,Good bye')
+        if any(text in statement for text in closeStatements):
+            speak('Do usłyszenia!')
+            print('Do usłyszenia!')
             break
 
-
-
-        if 'wikipedia' in statement:
-            speak('Searching Wikipedia...')
-            statement =statement.replace("wikipedia", "")
+        elif text := next((text for text in wikiStatements if text in statement), None):
+            speak('Przeszukuję Wikipedię...')
+            statement = statement.replace(text, "")
             results = wikipedia.summary(statement, sentences=3)
-            speak("According to Wikipedia")
+            speak("Według wikipedii")
             print(results)
             speak(results)
 
-        elif 'open youtube' in statement:
+        elif any(text in statement for text in youtubeStatements):
             webbrowser.open_new_tab("https://www.youtube.com")
-            speak("youtube is open now")
+            speak("Otworzono youtube")
             time.sleep(5)
 
-        elif 'open google' in statement:
+        elif any(text in statement for text in googleStatements):
             webbrowser.open_new_tab("https://www.google.com")
-            speak("Google chrome is open now")
+            speak("Otwieram stronę google.com")
             time.sleep(5)
 
-        elif 'open gmail' in statement:
+        elif any(text in statement for text in mailStatements):
             webbrowser.open_new_tab("gmail.com")
-            speak("Google Mail open now")
+            speak("Otworzono gmail")
             time.sleep(5)
 
-        elif "weather" in statement:
+        elif any(text in statement for text in weatherStatements):
             api_key="8ef61edcf1c576d65d836254e11ea420"
             base_url="https://api.openweathermap.org/data/2.5/weather?"
-            speak("whats the city name")
+            speak("W jakim mieście?")
             city_name=takeCommand()
             complete_url=base_url+"appid="+api_key+"&q="+city_name
             response = requests.get(complete_url)
@@ -105,55 +117,48 @@ if __name__=='__main__':
                 current_temperature = y["temp"]
                 current_humidiy = y["humidity"]
                 z = x["weather"]
-                weather_description = z[0]["description"]
-                speak(" Temperature in kelvin unit is " +
-                      str(current_temperature) +
-                      "\n humidity in percentage is " +
-                      str(current_humidiy) +
-                      "\n description  " +
-                      str(weather_description))
-                print(" Temperature in kelvin unit = " +
-                      str(current_temperature) +
-                      "\n humidity (in percentage) = " +
-                      str(current_humidiy) +
-                      "\n description = " +
-                      str(weather_description))
+                description_translated = None
+                # if z[0]["description"] is not None:
+                #     print(z[0]["description"])
+                #     description_translated = translator.translate(z[0]["description"], src='en', dest='pl')
+                speak(f'Temperatura wynosi: {int(current_temperature - 273.15)}')
+                speak(f'Procentowa wilgotność wynosi: {current_humidiy}')
+                speak(f'Opis: {z[0]["description"]}')
+                # if description_translated is not None:
+                #     speak(f'Opis: {description_translated}')
 
             else:
-                speak(" City Not Found ")
+                speak(" Nie znaleziono miasta")
 
-
-
-        elif 'time' in statement:
+        elif any(text in statement for text in timeStatements):
             strTime=datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"the time is {strTime}")
+            speak(f"Obecna godzina: {strTime}")
 
-        elif 'who are you' in statement or 'what can you do' in statement:
-            speak('I am Jester version 1 point O your persoanl assistant. I am programmed to minor tasks like'
-                  'opening youtube,google chrome,gmail and stackoverflow ,predict time,take a photo,search wikipedia,predict weather' 
-                  'in different cities , get top headline news from times of india and you can ask me computational or geographical questions too!')
+        elif any(text in statement for text in whoStatements):
+            speak('Jestem Grażyna dwatysiące - twój asysten głosowy. Potrafię wykonywać takie czynności jak'
+                  'otworzyć youtube, google, gmail czy stack overflow, zwracać czas, przeszukiwać wikipedię i zwracać aktualną pogodę w danym mieście' 
+                  'zwracam newsy z kraju oraz radzę sobie z obliczeniami i pytaniami geograficznymi')
 
+        elif any(text in statement for text in buildStatements):
+            speak("Zostałam zbudowana przez Huberta, Marcina i Mateusza")
+            print("Zostałam zbudowana przez Huberta, Marcina i Mateusza")
 
-        elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
-            speak("I was built by Hubert")
-            print("I was built by Hubert")
-
-        elif "open stackoverflow" in statement:
+        elif any(text in statement for text in stackStatements):
             webbrowser.open_new_tab("https://stackoverflow.com/login")
-            speak("Here is stackoverflow")
+            speak("Otworzono stack overflow")
 
-        elif 'news' in statement:
-            news = webbrowser.open_new_tab("https://www.europarl.europa.eu/news/pl")
-            speak('Here are some headlines from the european parlament,Happy reading')
+        elif any(text in statement for text in newsStatements):
+            news = webbrowser.open_new_tab("https://news.google.com/topstories?hl=pl&gl=PL&ceid=PL:pl")
+            speak('Oto kilka newsów zaproponowanych przez Google. Miłego czytania')
             time.sleep(6)
 
-        elif 'search'  in statement:
-            statement = statement.replace("search", "")
+        elif text := next((text for text in searchStatements if text in statement), None):
+            statement = statement.replace(text, "")
             webbrowser.open_new_tab(statement)
             time.sleep(5)
 
-        elif 'ask' in statement:
-            speak('I can answer to computational and geographical questions and what question do you want to ask now')
+        elif 'zapytaj' in statement:
+            speak('Potrafię odpowiedzieć na pytania obliczeniowe i geograficzne. Co chcesz wiedzieć?')
             question=takeCommand()
             app_id="R2K75H-7ELALHR35X"
             client = wolframalpha.Client('R2K75H-7ELALHR35X')
@@ -162,9 +167,9 @@ if __name__=='__main__':
             speak(answer)
             print(answer)
 
-
-        elif "log off" in statement or "sign out" in statement:
-            speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
+        elif any(text in statement for text in logoutStatements):
+            speak("Ok, twój komputer wyłączy się za 10 sekund, upewnij się, że zamknąłeś i zapisałeś stan we wszystkich aplikacjach")
             subprocess.call(["shutdown", "/l"])
+
 
 time.sleep(3)
